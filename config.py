83b82aea726cc78510e6b03c267a7476bfc9cabf
Bobby McGetrick <ramjr13@gmail.com>
2024-05-21 15:24:40 -0400

    initial commit

---
+++ config.py
@@ -0,0 +1,88 @@
+DEFAULT_CONTRACTIONS = {
+    "can't": "cannot",
+    "won't": "will not",
+    "aren't": "are not",
+    "isn't": "is not",
+    "don't": "do not",
+    "doesn't": "does not",
+    "didn't": "did not",
+    "haven't": "have not",
+    "hasn't": "has not",
+    "hadn't": "had not",
+    "shouldn't": "should not",
+    "wouldn't": "would not",
+    "couldn't": "could not",
+    "mustn't": "must not",
+    "mightn't": "might not",
+    "needn't": "need not",
+    "he's": "he is",
+    "she's": "she is",
+    "it's": "it is",
+    "they're": "they are",
+    "we're": "we are",
+    "you're": "you are",
+    "I'm": "I am",
+    "I'll": "I will",
+    "you'll": "you will",
+    "he'll": "he will",
+    "she'll": "she will",
+    "they'll": "they will",
+    "we'll": "we will",
+    "there's": "there is",
+    "here's": "here is",
+    "what's": "what is",
+    "that's": "that is",
+    "who's": "who is",
+    "how's": "how is",
+    "why's": "why is",
+    "let's": "let us",
+    "we've": "we have",
+    "they've": "they have",
+    "you've": "you have",
+    "I've": "I have",
+    "isn't": "is not",
+    "didn't": "did not",
+    "can't": "cannot",
+    "won't": "will not",
+    "could've": "could have",
+    "should've": "should have",
+    "would've": "would have"
+}
+
+
+DEFAULT_TECHNICAL_TERMS = {
+    'computer_science': {'algorithm', 'binary', 'compiler', 'middleware'},
+    'finance': {'amortization', 'capital', 'hedge', 'leverage'},
+    'medicine': {'biopsy', 'anemia', 'cardiomyopathy', 'dialysis'},
+    'medical_insurance': {'premium', 'deductible', 'copayment', 'coinsurance', 'claim', 'network', 'out-of-pocket', 'HMO', 'PPO', 'EPO', 'POS'},
+    'health_care': {'primary_care', 'specialist', 'urgent_care', 'emergency_room', 'inpatient', 'outpatient', 'telehealth', 'preventive_care'},
+    'pharmacy': {'prescription', 'over-the-counter', 'generic', 'brand-name', 'pharmacokinetics', 'dosage', 'contraindication', 'side_effect'},
+    'related': {'rehabilitation', 'palliative_care', 'mental_health', 'chronic_disease', 'acute_care', 'long_term_care'}
+}
+
+
+DEFAULT_KNOWLEDGE_BASE = {
+    'World_Health_Organization': 'Q7817',
+    'Centers_for_Disease_Control_and_Prevention': 'Q180686',
+    'American_Medical_Association': 'Q267395',
+    'Pfizer': 'Q130074',
+    'Merck': 'Q646731',
+    'UnitedHealth_Group': 'Q786391',
+    'CVS_Health': 'Q17114860',
+    'Walgreens_Boots_Alliance': 'Q482782'
+}
+
+
+DEFAULT_CONSOLIDATION_RULES = {
+    'IBM': ['International Business Machines', 'IBM Corp'],
+    'Google': ['Google Inc.', 'Alphabet Inc.'],
+    'World_Health_Organization': ['WHO', 'World Health Org'],
+    'Centers_for_Disease_Control_and_Prevention': ['CDC', 'Centers for Disease Control'],
+    'American_Medical_Association': ['AMA', 'American Medical Assoc'],
+    'Pfizer': ['Pfizer Inc.', 'Pfizer Pharmaceuticals'],
+    'Merck': ['Merck & Co.', 'Merck Sharp & Dohme'],
+    'UnitedHealth_Group': ['United Health Group', 'UnitedHealth'],
+    'CVS_Health': ['CVS', 'CVS Pharmacy', 'CVS Health Corporation'],
+    'Walgreens_Boots_Alliance': ['Walgreens', 'Walgreens Boots', 'Walgreens Alliance'],
+    'Optum': ['Optum', 'OptumRX', 'Optum RX']
+}
